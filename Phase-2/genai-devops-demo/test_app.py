9c7038341af937d9cb20f4d073db721b89e19bb2
Chethan K P <chethankp@Chethans-MacBook-Pro.local>
2026-09-23 17:06:38 +0530

    test

--- Phase-2/genai-devops-demo/test_app.py
+++ Phase-2/genai-devops-demo/test_app.py
@@ -3,8 +3,8 @@ from app import add, multiply
 def test_add():
     assert add(2, 3) == 5
 
-def test_multiply():
-    assert multiply(2, 3) == 6. #pass
-
 # def test_multiply():
-#     assert multiply(2, 3) == 5.  #fail
+#     assert multiply(2, 3) == 6. #pass
+
+def test_multiply():
+    assert multiply(2, 3) == 5.  #fail
